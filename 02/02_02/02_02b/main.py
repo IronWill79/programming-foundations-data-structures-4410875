bd6d793e2dee4a93ee53be3b9b29c2ddb3d82262
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-12-11 10:08:31 +1000

    Chapter 2 part 2

--- 02/02_02/02_02b/main.py
+++ 02/02_02/02_02b/main.py
@@ -2,3 +2,15 @@
 Problem Statement: Compute the average number of pets each student 
 has in a given class. 
 '''
+
+student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
+total_students = len(student_pet_count_list)
+total_pets = 0
+
+for count in student_pet_count_list:
+  total_pets += count
+
+average_pets = total_pets / total_students
+
+print(average_pets)
+
